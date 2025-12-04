6509dfd7bd1c50db425ef76d34d4840b73a3e5ba
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-12-04 16:53:56 +0700

    Percobaan 4

---
+++ PengunjungCafe15.java
@@ -0,0 +1,19 @@
+public class PengunjungCafe15 {
+
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+        System.out.println(); // supaya ada jarak output
+    }
+
+    // Fungsi main
+    public static void main(String[] args) {
+
+        daftarPengunjung("Ali", "Budi", "Citra");
+
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
