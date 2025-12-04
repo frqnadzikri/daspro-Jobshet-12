b1f75fb892493fd69e2a86a203ad67aea1d4d1f0
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-12-04 16:20:43 +0700

    Percobaan 2

--- Kafe15.java
+++ Kafe15.java
@@ -1,7 +1,12 @@
 public class Kafe15 {
     
-    public static void main(String[] args) {
-        // Menampilkan menu langsung di main tanpa fungsi Menu()
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+        
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -11,5 +16,9 @@ public class Kafe15 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+    
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
